650df012a7dbe070888b660b79eb9ef3a6446371
Alex Mantaut <19215583+alexmantaut@users.noreply.github.com>
2023-09-22 14:44:35 +0200

    Fixed issue with prisma crud library

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PrismaCrudModule } from 'nestjs-prisma-crud';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    /**
+     *  PrismaCrudModule registers the PrismaService provider globally.
+     *  No need to provide it anywhere else!
+     */
+    PrismaCrudModule.register({
+      prismaService: PrismaService,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
