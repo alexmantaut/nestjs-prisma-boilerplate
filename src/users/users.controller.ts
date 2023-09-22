9b912d1f227b5dccd7125fcb911a3b78236746b9
Alex Mantaut <19215583+alexmantaut@users.noreply.github.com>
2023-09-22 16:03:07 +0200

    Added swagger doc

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,12 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UserDto } from './dto/user.dto';
 
 @ApiTags('Users')
@@ -20,6 +25,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create new user' })
   @ApiOkResponse({ type: UserDto })
   @ApiCreatedResponse({ description: 'Successfully created entity' })
   create(@Body() createUserDto: CreateUserDto) {
@@ -27,24 +33,28 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get user list' })
   @ApiOkResponse({ type: UserDto, isArray: true })
   findAll(@Query('crudQuery') crudQuery: string) {
     return this.usersService.findMany({ crudQuery });
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get information for a user' })
   @ApiOkResponse({ type: UserDto })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a user' })
   @ApiOkResponse({ type: UserDto })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user' })
   @ApiOkResponse({})
   remove(@Param('id') id: string) {
     return this.usersService.remove(id, null);
