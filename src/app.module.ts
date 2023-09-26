469855e2db037e46fc8d0c1ccea2e3c9c9616509
Alex Mantaut <19215583+alexmantaut@users.noreply.github.com>
2023-09-26 12:44:46 +0200

    Added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthzModule } from './authz/authz.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
     UsersModule,
+    ConfigModule.forRoot({}),
     /**
      *  PrismaCrudModule registers the PrismaService provider globally.
      *  No need to provide it anywhere else!
